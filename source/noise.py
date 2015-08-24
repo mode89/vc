926f56ce5b56878ede0e3c072a55a3d939f7aefc
Mode <mode89@mail.ru>
2015-08-24 20:02:21 +0800

    vc : plot histogram of noise

--- source/noise.py
+++ source/noise.py
@@ -1,3 +1,6 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import struct
 import wave
@@ -5,7 +8,15 @@ import wave
 NOISE_PATH = os.path.join(
     os.path.dirname( os.path.realpath( __file__ ) ), "data/noise.wav" )
 noise = wave.open( NOISE_PATH, "r" )
+frame_rate = noise.getframerate()
 frames = []
 for i in range( noise.getnframes() ) :
     frame = noise.readframes( 1 )
     frames.append( struct.unpack( "<h", frame ) )
+ft = np.fft.fft( frames[ 0 : ( frame_rate / 10 ) ], axis=0 )
+freq = np.fft.fftfreq( len( ft ), 1.0 / frame_rate )
+plt.bar(
+    left=map( math.log10, freq[ 1 : 100 ] ),
+    height=map( abs, ft[ 1 : 100 ] ),
+    align="center", width=0.1 )
+plt.show()
