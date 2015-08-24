08368871fd62e0291bf5b29b2fd01d5b024219b9
Mode <mode89@mail.ru>
2015-08-24 18:07:30 +0800

    vc : read noise.wav

--- source/noise.py
+++ source/noise.py
@@ -1,6 +1,11 @@
 import os
+import struct
 import wave
 
 NOISE_PATH = os.path.join(
     os.path.dirname( os.path.realpath( __file__ ) ), "data/noise.wav" )
 noise = wave.open( NOISE_PATH, "r" )
+frames = []
+for i in range( noise.getnframes() ) :
+    frame = noise.readframes( 1 )
+    frames.append( struct.unpack( "<h", frame ) )
