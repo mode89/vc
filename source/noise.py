ccb981f769950e949190daaf38be75e20d88f5ce
Mode <mode89@mail.ru>
2015-08-26 08:43:55 +0800

    vc : initialize noise model
    Read wave file and find min, max of FFT.

--- source/noise.py
+++ source/noise.py
@@ -1,61 +1,37 @@
-import math
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import struct
-import threading
 import wave
 
-NOISE_PATH = os.path.join(
+WAVE_FILE_PATH = os.path.join(
     os.path.dirname( os.path.realpath( __file__ ) ), "data/noise.wav" )
-BAR_COUNT = 100
 SAMPLE_RATE = 100
 
-noise = wave.open( NOISE_PATH, "r" )
-frame_rate = noise.getframerate()
-FRAMES_PER_SAMPLE = int( frame_rate / SAMPLE_RATE )
-frames = []
-for i in range( noise.getnframes() ) :
-    frame = noise.readframes( 1 )
-    frames.append( struct.unpack( "<h", frame ) )
-
-class Worker( threading.Thread ) :
+class Model :
 
     def __init__( self ) :
-        threading.Thread.__init__( self )
-        self._event_stop = threading.Event()
 
-    def run( self ) :
-        while not self._event_stop.isSet():
-            for i in range( 0, noise.getnframes(), FRAMES_PER_SAMPLE ) :
-                self.fft = np.fft.fft(
-                    frames[ i : i + FRAMES_PER_SAMPLE ], axis=0 )
+        # Read wavefront file
 
-    def stop( self ) :
-        self._event_stop.set()
+        wave_file = wave.open( WAVE_FILE_PATH, "r" )
+        frame_rate = wave_file.getframerate()
+        frame_count = wave_file.getnframes()
+        frames = np.fromstring( string=wave_file.readframes( frame_count ),
+            dtype=np.int16 ) / float( np.iinfo( np.int16 ).max )
+        frames_per_sample = int( frame_rate / SAMPLE_RATE )
+        fft_freq_count = frames_per_sample / 2
 
-worker = Worker()
-worker.start()
+        # Find minimums and maximums of FFT
 
-freq = np.fft.fftfreq( FRAMES_PER_SAMPLE, 1.0 / frame_rate )
-ft = np.fft.fft( frames[ 0 : FRAMES_PER_SAMPLE ], axis=0 )
+        self.fft_min = np.full( fft_freq_count - 1, np.Inf )
+        self.fft_max = np.zeros( fft_freq_count - 1 )
 
-figure = plt.figure()
-plotBars = plt.bar(
-    # left=map( math.log10, freq[ 1 : BAR_COUNT ] ),
-    left = freq[ 1 : BAR_COUNT ],
-    height=list( map( abs, ft[ 1 : BAR_COUNT ] ) ),
-    align="center", width=0.01 )
-plt.ylim(10, 300000)
-
-def animateFunction( sampleId ) :
-    for i, bar in enumerate( plotBars ) :
-        bar.set_height( abs( worker.fft[i + 1] ) )
-
-anim = animation.FuncAnimation( figure, animateFunction, interval=100 )
-
-plt.show()
-
-worker.stop()
-worker.join()
+        frame_range = range( 0, frame_count - frames_per_sample,
+            frames_per_sample )
+        for first_frame in frame_range:
+            last_frame = first_frame + frames_per_sample
+            fft = np.fft.fft( frames[ first_frame : last_frame ], axis=0 )
+            abs_fft = np.abs( fft[ 1 : fft_freq_count ] ) / \
+                frames_per_sample
+            self.fft_min = np.minimum( self.fft_min, abs_fft )
+            self.fft_max = np.maximum( self.fft_max, abs_fft )
