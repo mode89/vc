9e9b4210ddc9428796ac34672d2dcab7d4bfcca7
Mode <mode89@mail.ru>
2015-08-27 19:15:18 +0800

    vc : step trainer

--- source/vc.py
+++ source/vc.py
@@ -1,8 +1,13 @@
 import esn
+import training
 
 NEURON_COUNT = 100
 INPUT_COUNT = 100
 CONNECTIVITY = 0.5
+SIM_STEP = 0.01
+TRAIN_TIME = 100.0
+
+# Create network
 
 network = esn.create_network(
     inputCount=INPUT_COUNT,
@@ -11,3 +16,10 @@ network = esn.create_network(
     connectivity=CONNECTIVITY,
     useOrthonormalMatrix=True
 )
+
+# Train the network
+
+trainer = training.Trainer(network)
+
+for i in range(int(TRAIN_TIME / SIM_STEP)) :
+    trainer.step(SIM_STEP)
