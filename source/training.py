655ec03d170540746a0ecd714752afe910dcbbee
Mode <mode89@mail.ru>
2015-08-27 19:16:26 +0800

    vc : update inputs of network during training
    Initial implementation of class Inputs. Declared states
    Inputs.PlaySample and Inputs.PlayNoise.

--- source/training.py
+++ source/training.py
@@ -1,7 +1,33 @@
+class Inputs:
+
+    def __init__(self):
+        self.state = Inputs.PlaySample()
+
+    def __call__(self):
+        assert 0, "Not yet implemented"
+
+    def update(self, step):
+        self.state.update(step)
+
+    class State: pass
+
+    class PlaySample(State):
+
+        def update(self, step):
+            assert 0, "Not yet implemented"
+
+    class PlayNoise(State):
+
+        def update(self, step):
+            assert 0, "Not yet implemented"
+
 class Trainer:
 
     def __init__(self, network):
         self.network = network
+        self.inputs = Inputs()
 
     def step(self, step):
+        self.inputs.update(step)
+        self.network.set_inputs(self.inputs())
         self.network.step(step)
