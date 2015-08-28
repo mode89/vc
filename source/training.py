dadefca71782154fcbc979f54828cf0c40e9a997
Mode <mode89@mail.ru>
2015-08-28 19:26:45 +0800

    vc : implement input's state changing

--- source/training.py
+++ source/training.py
@@ -1,6 +1,8 @@
 import random
 import samples
 
+NOISE_LENGTH_RANGE = (0.5, 2.0)
+
 class Inputs:
 
     def __init__(self):
@@ -10,7 +12,11 @@ class Inputs:
         assert 0, "Not yet implemented"
 
     def update(self, step):
-        self.state.update(step)
+        self.frames = self.state.read_frames(step)
+        if self.frames is None:
+            self.state = self.state.next()
+            self.frames = self.state.read_frames(step)
+        assert self.frames is not None, "There are must be some frames"
 
     class State: pass
 
@@ -18,14 +24,33 @@ class Inputs:
 
         def __init__(self):
             self.sample = random.choice(samples.SAMPLES)
+            self.sample.reset()
 
-        def update(self, step):
-            frames = self.sample.read_frames(step)
+        def read_frames(self, step):
+            return self.sample.read_frames(step)
+
+        def next(self):
+            return Inputs.PlayNoise()
 
     class PlayNoise(State):
 
-        def update(self, step):
-            assert 0, "Not yet implemented"
+        def __init__(self):
+            self.sample = samples.NOISE
+            self.length = random.uniform(*NOISE_LENGTH_RANGE)
+
+        def read_frames(self, step):
+            self.length -= step
+            if self.length < 0:
+                return None
+            frames = self.sample.read_frames(step)
+            if frames is None:
+                self.sample.reset()
+                frames = self.sample.read_frames(step)
+                assert frames is not None, "There are must be some frames"
+            return frames
+
+        def next(self):
+            return Inputs.PlaySample()
 
 class Trainer:
 
