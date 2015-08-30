eab06c0edc7ebf68a0b6cf787c5168df25b58d52
Mode <mode89@mail.ru>
2015-08-30 15:59:36 +0800

    vc : generate training output signal

--- source/training.py
+++ source/training.py
@@ -1,20 +1,27 @@
 from scikits.talkbox.features import mfcc
 import random
 import samples
+import signals
 
 NOISE_LENGTH_RANGE = (0.5, 2.0)
+OUTPUT_AMPLITUDE = 0.7
+OUTPUT_PULSE_WIDTH = 0.1
 
 class Inputs:
 
     def __init__(self):
         self.state = PlaySample()
+        self.end_of_sample = False
 
     def __call__(self):
         return self.mfcc
 
     def update(self, step):
+        self.end_of_sample = False
         self.frames = self.state.read_frames(step)
         if self.frames is None:
+            if isinstance(self.state, PlaySample):
+                self.end_of_sample = True
             self.state = self.state.next()
             self.frames = self.state.read_frames(step)
         assert self.frames is not None, "There are must be some frames"
@@ -53,15 +60,35 @@ class PlayNoise():
     def next(self):
         return PlaySample()
 
+class Outputs:
+
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.pulse = self.new_pulse()
+        self.pulse_time = 0
+
+    def update(self, step):
+        if self.inputs.end_of_sample:
+            self.pulse = self.new_pulse()
+            self.pulse_time = 0
+        self.value = self.pulse(self.pulse_time)
+        self.pulse_time += step
+
+    def new_pulse(self):
+        return signals.GaussianPulse(
+            amplitude=OUTPUT_AMPLITUDE, width=OUTPUT_PULSE_WIDTH)
+
 class Trainer:
 
     def __init__(self, network):
         self.network = network
         self.inputs = Inputs()
+        self.outputs = Outputs(self.inputs)
         self.time = 0
 
     def step(self, step):
         self.time += step
         self.inputs.update(step)
+        self.outputs.update(step)
         self.network.set_inputs(self.inputs())
         self.network.step(step)
