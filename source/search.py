b7eec23f0ae2a9792e0c3816b525bc4d0c9b1220
Mode <mode89@mail.ru>
2015-09-01 22:43:44 +0800

    vc : refactor search.py
    Replace class Model with function simulation().

--- source/search.py
+++ source/search.py
@@ -5,30 +5,31 @@ import training
 
 SIM_STEP = 0.01
 
-class Model:
+def simulation(neuron_count, connectivity, leaking_rate, input_scale,
+    feedback_scale, washout_time, train_time):
 
-    def __init__(self, neuron_count, connectivity, leaking_rate,
-        input_scale, feedback_scale, washout_time, train_time):
+    network = esn.create_network(
+        inputCount=13,
+        neuronCount=neuron_count,
+        outputCount=1,
+        connectivity=connectivity,
+        leakingRate=leaking_rate,
+        useOrthonormalMatrix=True)
 
-        network = esn.create_network(
-            inputCount=13,
-            neuronCount=neuron_count,
-            outputCount=1,
-            connectivity=connectivity,
-            leakingRate=leaking_rate,
-            useOrthonormalMatrix=True)
+    input_scalings = numpy.ones(13) * input_scale
+    input_scalings[0] *= 0.03
+    network.set_input_scalings(input_scalings)
+    network.set_feedback_scalings([feedback_scale])
 
-        input_scalings = numpy.ones(13) * input_scale
-        input_scalings[0] *= 0.03
-        network.set_input_scalings(input_scalings)
-        network.set_feedback_scalings([feedback_scale])
+    trainer = training.Trainer(network, washout_time=washout_time,
+        train_time=train_time)
 
-        self.trainer = training.Trainer(network, washout_time=washout_time,
-            train_time=train_time)
+    while trainer.time < trainer.train_time:
+        trainer.step(SIM_STEP)
 
-    def run(self):
-        while self.trainer.time < self.trainer.train_time:
-            self.trainer.step(SIM_STEP)
+def simulation_star(params):
+    print(params)
+    simulation(**params)
 
 if __name__ == "__main__":
 
@@ -42,7 +43,4 @@ if __name__ == "__main__":
             train_time=[500.0, 1000.0, 100.0]
         ))
 
-    for params in grid:
-        print(params)
-        model = Model(**params)
-        model.run()
+    map(simulation_star, grid)
