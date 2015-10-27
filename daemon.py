eb82dc48ab3ada892140923cbf4e100e3637e4eb
Mode <mode89@mail.ru>
2015-10-27 09:55:22 +0800

    vc : initial implementation of Daemon
    Create network, run simulation.

---
+++ daemon.py
@@ -0,0 +1,23 @@
+import esn
+import input
+
+INPUT_COUNT = 13
+NEURON_COUNT = 100
+CONNECTIVITY = 0.5
+
+class Daemon:
+
+    def __init__(self):
+        print("Generating network...")
+        self.network = esn.create_network(
+            inputCount=INPUT_COUNT,
+            neuronCount=NEURON_COUNT,
+            outputCount=1,
+            connectivity=CONNECTIVITY,
+            useOrthonormalMatrix=True)
+        self.input_audio = input.Audio()
+
+    def loop(self):
+        while True:
+            self.network.set_inputs(self.input_audio.read())
+        self.input.close()
--- vc.py
+++ vc.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python
+from daemon import Daemon
 import argparse
-import esn
-import input
-
-INPUT_COUNT = 13
-NEURON_COUNT = 100
-CONNECTIVITY = 0.5
 
 COMMANDS = ["daemonize"]
 
 def daemonize():
-    print("Generating network...")
-    network = esn.create_network(
-        inputCount=INPUT_COUNT,
-        neuronCount=NEURON_COUNT,
-        outputCount=1,
-        connectivity=CONNECTIVITY,
-        useOrthonormalMatrix=True
-    )
-
-    input_audio = input.Audio()
-
-    while True:
-        network.set_inputs(input_audio.read())
-
-    input.close()
+    daemon = Daemon()
+    daemon.loop()
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
