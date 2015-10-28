44cc5f6a1a03599fc4af387aa1d5c2f3f0a0466b
Mode <mode89@mail.ru>
2015-10-28 11:44:00 +0800

    vc : implement server as SocketServer

--- communication.py
+++ communication.py
@@ -1,9 +1,25 @@
+from SocketServer import BaseRequestHandler
+from SocketServer import TCPServer
+from SocketServer import ThreadingMixIn
 from threading import Thread
 
-class Server(Thread):
+SERVER_ADDRESS = "localhost", 0
+
+class Server(ThreadingMixIn, TCPServer):
 
     def __init__(self):
-        Thread.__init__(self)
+        TCPServer.__init__(self, SERVER_ADDRESS, Server.RequestHandler)
+        self.thread = Thread(target=self.serve_forever)
+        self.thread.daemon = True
 
-    def run(self):
-        pass
+    def start(self):
+        self.thread.start()
+
+    def shutdown(self):
+        TCPServer.shutdown(self)
+        TCPServer.server_close(self)
+
+    class RequestHandler(BaseRequestHandler):
+
+        def handle(self):
+            pass
