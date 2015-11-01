e7c0c251276baefbd9961432c44a7c1ad95c7950
Mode <mode89@mail.ru>
2015-11-01 14:50:04 +0800

    vc : implement Server as SimpleXMLRPCServer

--- server.py
+++ server.py
@@ -1,30 +1,20 @@
-from SocketServer import BaseRequestHandler
-from SocketServer import TCPServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SocketServer import ThreadingMixIn
 from threading import Thread
 
 ADDRESS = "localhost", 1234
 
-class Server(ThreadingMixIn, TCPServer):
+class Server(ThreadingMixIn, SimpleXMLRPCServer):
 
     def __init__(self, daemon):
-        TCPServer.__init__(self, ADDRESS, Server.RequestHandler)
+        SimpleXMLRPCServer.__init__(self, ADDRESS)
         self.thread = Thread(target=self.serve_forever)
         self.thread.daemon = True
-        self.daemon = daemon
+        self.register_instance(daemon)
 
     def start(self):
         self.thread.start()
 
     def shutdown(self):
-        TCPServer.shutdown(self)
-        TCPServer.server_close(self)
-
-    class RequestHandler(BaseRequestHandler):
-
-        def handle(self):
-            data = self.request.recv(1024)
-            args = data.split()
-            command = args[0]
-            options = args[1:]
-            getattr(self.server.daemon, command)(options)
+        SimpleXMLRPCServer.shutdown(self)
+        SimpleXMLRPCServer.server_close(self)
