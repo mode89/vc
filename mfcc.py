6c6a3d5e63d5c0e53a7fdd858cdef41e4a1479c5
Mode <mode89@mail.ru>
2016-04-07 19:54:17 +0800

    specify number of coeffs when calculate MFCC

--- mfcc.py
+++ mfcc.py
@@ -1,7 +1,6 @@
 import scikits.talkbox.features as scikits
 
 FRAME_RATE = 44100
-COEFF_NUM = 13
 
-def mfcc(frames):
-    return scikits.mfcc(frames, fs=FRAME_RATE, nceps=COEFF_NUM)[0][0]
+def mfcc(frames, num_coeff):
+    return scikits.mfcc(frames, fs=FRAME_RATE, nceps=num_coeff)[0][0]
