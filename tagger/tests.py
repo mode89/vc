7f07790d0d642bf7450a01d8fbd10df9856083a5
Mode <mode89@mail.ru>
2016-09-07 00:04:04 +0800

    tagger: test button

--- tagger/tests.py
+++ tagger/tests.py
@@ -17,5 +17,16 @@ class TaggerTests(unittest.TestCase):
             return flask.render_template("hello.html")
         app.run(host="0.0.0.0")
 
+    def test_button(self):
+        app = flask.Flask(__name__)
+        @app.route("/")
+        def hello():
+            return flask.render_template("button.html")
+        @app.route("/button", methods=["POST"])
+        def button():
+            print("Pressed button")
+            return flask.redirect("/")
+        app.run(host="0.0.0.0")
+
 if __name__ == "__main__":
     unittest.main()
