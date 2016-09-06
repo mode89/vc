72483319a3e12958f5188f3488d661f5ad339218
Mode <mode89@mail.ru>
2016-09-06 23:31:45 +0800

    tagger: test template rendering

--- tagger/tests.py
+++ tagger/tests.py
@@ -3,11 +3,18 @@ import unittest
 
 class TaggerTests(unittest.TestCase):
 
-    def test_flask_hello_world(self):
+    def test_hello_world(self):
         app = flask.Flask(__name__)
         @app.route("/")
         def hello_world():
             return "Hello, World!"
+        app.run(host="0.0.0.0")
+
+    def test_render_template(self):
+        app = flask.Flask(__name__)
+        @app.route("/")
+        def hello():
+            return flask.render_template("hello.html")
         app.run(host="0.0.0.0")
 
 if __name__ == "__main__":
